d8dc2b654dd5e7f6cc3da31751745c91d99dc7ba
brwhale <lazorpwnage@gmail.com>
2020-07-08 16:01:58 -0700

    adds features

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,18 +2,75 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace VirtualMasterController
 {
+    public class ShowListing
+    {
+        public string Title { get; set; }
+        public string CountStr { get { return "" + episodePaths.Count + " episodes"; } }
+        public int Count { get { return episodePaths.Count; } }
+        public List<string> episodePaths = new List<string>();
+    }
+
+    public class countTotal
+    {
+        public int index = 0;
+        public int total;
+        public double completion { get { return (double)index / (double)total; } }
+
+        public countTotal(int total_)
+        {
+            total = total_;
+        }
+    }
+
     public partial class MainWindow : Window
     {
-        List<string> episodePaths = new List<string>();
+        public List<ShowListing> Shows = new List<ShowListing>();
         int scrollIndex = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string getPlaylist(string delim, int skipN = 0)
+        {
+            double avgCompletion = 0.0;
+            string playlist = "";
+            List<countTotal> showIndex = new List<countTotal>();
+            foreach (var show in Shows)
+            {
+                showIndex.Add(new countTotal(show.Count));
+            }
+            while (avgCompletion != 1.0)
+            {
+                for (int i = 0; i < Shows.Count; i++)
+                {
+                    if (showIndex[i].index < showIndex[i].total && showIndex[i].completion <= avgCompletion)
+                    {
+                        if (skipN == 0)
+                        {
+                            playlist += "\"" + Shows[i].episodePaths[showIndex[i].index] + "\"" + delim;
+                        } else
+                        {
+                            skipN--;
+                        }
+                        showIndex[i].index++;
+                    }
+                }
+
+                double complete = 0;
+                foreach (var show in showIndex)
+                {
+                    complete += show.completion;
+                }
+                avgCompletion = complete / showIndex.Count;
+            }
+            return playlist;
         }
 
         private void EpisodesTextBox_DragEnter(object sender, DragEventArgs e)
@@ -28,24 +85,39 @@ namespace VirtualMasterController
             }
         }
 
+        private void addItemOrDirtectory(ShowListing show, string fileName)
+        {
+            if (Directory.Exists(fileName))
+            {
+                foreach (var file2 in Directory.GetFiles(fileName))
+                {
+                    EpisodesTextBox.Text += file2 + "\n";
+                    show.episodePaths.Add(file2);
+                }
+
+                foreach (var file2 in Directory.GetDirectories(fileName))
+                {
+                    addItemOrDirtectory(show, file2);
+                }
+            }
+            else
+            {
+                EpisodesTextBox.Text += fileName + "\n";
+                show.episodePaths.Add(fileName);
+            }
+        }
+
         private void EpisodesTextBox_Drop(object sender, DragEventArgs e)
         {
             foreach (var fileName in (string[])e.Data.GetData(DataFormats.FileDrop, false))
             {
-                if (Directory.Exists(fileName))
-                {
-                    foreach (var file2 in Directory.GetFiles(fileName))
-                    {
-                        EpisodesTextBox.Text += file2 + "\n";
-                        episodePaths.Add(file2);
-                    }
-                }
-                else
-                {
-                    EpisodesTextBox.Text += fileName + "\n";
-                    episodePaths.Add(fileName);
-                }
-                
+                var show = new ShowListing();
+                show.Title = fileName.Split('\\').Last();
+
+                addItemOrDirtectory(show, fileName);
+                Shows.Add(show);
+                ShowListBox.Items.Add(show);
+                EpisodesTextBox.Text = getPlaylist("\n");
             }
         }
 
@@ -53,21 +125,13 @@ namespace VirtualMasterController
         {
             scrollIndex -= e.Delta/100;
             if (scrollIndex < 0) { scrollIndex = 0; }
-            EpisodesTextBox.Text = "";
-            for(int i = scrollIndex; i < episodePaths.Count; i++)
-            {
-                EpisodesTextBox.Text += episodePaths[i] + "\n";
-            }
+            EpisodesTextBox.Text = getPlaylist("\n", scrollIndex);
+  
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string playlist = "";
-            for (int i = 0; i < episodePaths.Count; i++)
-            {
-                playlist += "\"" + episodePaths[i] + "\" ";
-            }
-            System.Diagnostics.Process.Start("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", playlist);
+            System.Diagnostics.Process.Start("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", getPlaylist(" "));
         }
     }
 }
